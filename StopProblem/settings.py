7b4d587d0980e07f0aae275ef2433ad81944663b
mamalka <mamalka@cisco.com>
2021-06-08 13:24:23 +0300

    changed settings.py

--- StopProblem/settings.py
+++ StopProblem/settings.py
@@ -16,8 +16,6 @@ from configurations import Configuration
 
 
 class Base(Configuration):
-    # Build paths inside the project like this: BASE_DIR / 'subdir'.
-    BASE_DIR = Path(__file__).resolve().parent.parent
     # SECURITY WARNING: keep the secret key used in production secret!
     SECRET_KEY = 'django-insecure-7=qqlq%as8iv(46kkos4(#!_u7_yt0t3pv=emv!@i0iv@pe#!j'
 
@@ -45,22 +43,25 @@ class Base(Configuration):
     ]
     ROOT_URLCONF = 'StopProblem.urls'
 
-    TEMPLATES = [
-        {
-            'BACKEND': 'django.template.backends.django.DjangoTemplates',
-            'DIRS': [BASE_DIR / 'templates']
-            ,
-            'APP_DIRS': True,
-            'OPTIONS': {
-                'context_processors': [
-                    'django.template.context_processors.debug',
-                    'django.template.context_processors.request',
-                    'django.contrib.auth.context_processors.auth',
-                    'django.contrib.messages.context_processors.messages',
-                ],
+    @property
+    def TEMPLATES(self):
+        return [
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'DIRS': [os.path.join(self.BASE_DIR, 'templates')]
+                ,
+                'APP_DIRS': True,
+                'OPTIONS': {
+                    'context_processors': [
+                        'django.template.context_processors.debug',
+                        'django.template.context_processors.request',
+                        'django.contrib.auth.context_processors.auth',
+                        'django.contrib.messages.context_processors.messages',
+                    ],
+                },
             },
-        },
-    ]
+        ]
+
     WSGI_APPLICATION = 'StopProblem.wsgi.application'
 
     # Database
@@ -122,6 +123,8 @@ class Base(Configuration):
 
 
 class Develop(Base):
+    # Build paths inside the project like this: BASE_DIR / 'subdir'.
+    BASE_DIR = Path(__file__).resolve().parent.parent
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = True
     ALLOWED_HOSTS = ['127.0.0.1']
@@ -141,5 +144,6 @@ class Production(Base):
     # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
     STATICFILES_DIRS = [STATIC_ROOT]
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
