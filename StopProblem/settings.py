e26dd19e08eefbfd8ffa963025be8f87cffe5de6
mamalka <mamalka@cisco.com>
2021-06-08 13:38:02 +0300

    deploy to heroku

--- StopProblem/settings.py
+++ StopProblem/settings.py
@@ -143,7 +143,12 @@ class Production(Base):
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+    # Extra lookup directories for collectstatic to find static files
+    STATICFILES_DIRS = (
+        os.path.join(PROJECT_ROOT, 'static'),
+    )
 
-    STATICFILES_DIRS = [STATIC_ROOT]
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    #  Add configuration for static files storage using whitenoise
+    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
