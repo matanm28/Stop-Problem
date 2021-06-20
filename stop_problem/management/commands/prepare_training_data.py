f4787e25f09906b50949773dc58b4868389ffa53
mamalka <mamalka@cisco.com>
2021-06-20 16:31:04 +0300

    data gathering done

---
+++ stop_problem/management/commands/prepare_training_data.py
@@ -0,0 +1,15 @@
+from django.core.management import BaseCommand
+
+from stop_problem.machine_learning import main
+
+
+def Command():
+    return PrepareTrainingData()
+
+
+class PrepareTrainingData(BaseCommand):
+    def __init__(self, stdout=None, stderr=None, no_color=False, force_color=False):
+        super().__init__(stdout, stderr, no_color, force_color)
+
+    def handle(self, *args, **options):
+        main()
